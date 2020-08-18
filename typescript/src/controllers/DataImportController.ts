d1543774ab7ef64645576ae4be50d1dd853d844a
Wayne See <wayne.see@ufinity.com>
2020-08-18 20:38:12 +0800

    updated data import logic

--- typescript/src/controllers/DataImportController.ts
+++ typescript/src/controllers/DataImportController.ts
@@ -4,8 +4,7 @@ import Logger from '../config/logger';
 import upload from '../config/multer';
 import { ClassService, StudentService, SubjectService, TeacherService } from '../service';
 import { convertCsvToJson, isNewValue } from '../utils';
-import { TeacherClass, TeacherStudent, TeacherSubject } from '../models';
-import sequelize from '../config/database';
+import { TeacherStudent, TeacherSubject } from '../models';
 
 const DataImportController = Express.Router();
 const LOG = new Logger('DataImportController.js');
@@ -13,6 +12,7 @@ const LOG = new Logger('DataImportController.js');
 // TODO: Please implement Question 1 requirement here
 const dataImportHandler: RequestHandler = async (req, res, next) => {
 	const { file } = req;
+
 	const teacherService = new TeacherService();
 	const studentService = new StudentService();
 	const subjectService = new SubjectService();
@@ -23,26 +23,43 @@ const dataImportHandler: RequestHandler = async (req, res, next) => {
 		const data = await convertCsvToJson(file.path);
 		LOG.info(JSON.stringify(data, null, 2));
 
-		data.map(async (val) => {
-			const teacherRes = await teacherService.createTeacher(val.teacherName, val.teacherEmail);
-
-			if (!teacherRes.created && isNewValue(val.teacherName, teacherRes.teacher)) {
-				await teacherService.updateTeacher(val.teacherName, val.teacherEmail)
-			}
-			const studentRes = await studentService.createStudent(val.studentName, val.studentEmail);
-
-			if (!studentRes.created && isNewValue(val.studentName, studentRes.student)) {
-				await studentService.updateStudent(val.studentName, val.studentEmail)
+		for (const val of data) {
+			if (val.toDelete === "1") {
+				const teacherStudentQuery = {
+					where: {
+						teacherEmail: val.teacherEmail,
+						studentEmail: val.studentEmail
+					}
+				}
+				const teacherStudent = await TeacherStudent.findOne(teacherStudentQuery);
+				console.log(teacherStudent);
+				if (teacherStudent !== null) {
+					await TeacherStudent.destroy(teacherStudentQuery);
+				}
+				continue;
 			}
 			const subjectRes = await subjectService.createSubject(val.subjectName, val.subjectCode);
-
-			if (!subjectRes.created && isNewValue(val.subjectName, subjectRes.subject)) {
+			
+			if (!subjectRes.created && isNewValue(val.subjectName, "code", subjectRes.subject)) {
 				await subjectService.updateSubject(val.subjectName, val.subjectCode)
 			}
 			const classRes = await classService.createClass(val.className, val.classCode);
-
-			if (!classRes.created && isNewValue(val.className, classRes.classModel)) {
+			
+			if (!classRes.created && isNewValue(val.className, "code", classRes.classModel)) {
 				await classService.updateClass(val.className, val.classCode)
+			}
+			const teacherRes = await teacherService.createTeacher(val.teacherName, val.teacherEmail);
+
+			if (!teacherRes.created && isNewValue(val.teacherName, "name", teacherRes.teacher)) {
+				await teacherService.updateTeacher(val.teacherName, val.teacherEmail)
+			}
+			const studentRes = await studentService.createStudent(val.studentName, val.studentEmail, val.classCode);
+
+			if (!studentRes.created && isNewValue(val.studentName, "name", studentRes.student)) {
+				await studentService.updateStudent(val.studentName, val.studentEmail)
+			}
+			if (!studentRes.created && isNewValue(val.classCode, "classCode", studentRes.student )) {
+				await studentService.updateStudentClass(val.classCode, val.studentEmail)
 			}
 
 			await TeacherStudent.findOrCreate({
@@ -52,27 +69,19 @@ const dataImportHandler: RequestHandler = async (req, res, next) => {
 				}
 			});
 
-			await TeacherClass.findOrCreate({
-				where: {
-					teacherEmail: teacherRes.teacher.getDataValue('email'),
-					classCode: classRes.classModel.getDataValue('code'),
-				}
-			});
 			await TeacherSubject.findOrCreate({
 				where: {
 					teacherEmail: teacherRes.teacher.getDataValue('email'),
 					subjectCode: subjectRes.subject.getDataValue('code'),
 				}
 			});
-		});
+		};
 
 	} catch (err) {
 		// LOG.error(err)
 		console.log(">>>>>>", err)
 		return next(err);
 	}
-
-	console.log("test", await TeacherClass.findOne({ where: { teacherEmail: 'teacher1@gmail.com' } }))
 
 	return res.sendStatus(NO_CONTENT);
 }
